dcf9cdf0590faae0492a35f137dcb7a33de7d990
dwol87 <dwolinsky87@gmail.com>
2022-11-30 22:45:31 -0500

    commit it

---
+++ application/views/templates/header.php
@@ -0,0 +1,23 @@
+<html>
+
+<head>
+    <title>ciBlog</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootswatch@4.5.2/dist/flatly/bootstrap.min.css" integrity="sha384-qF/QmIAj5ZaYFAeQcrQ6bfVMAh4zZlrGwTPY7T/M+iTTLJqJBJjwwnsE5Y0mV7QK" crossorigin="anonymous">
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg bg-primary">
+        <div class="container">
+            <div class="navbar-header">
+                <a href="#" class="navbar-brand">ciBlog</a>
+            </div>
+            <div id="navbar">
+                <ul class="nav navbar-nav">
+                    <li><a href="<?php echo base_url(); ?>">Home</a></li>
+                    <li><a href="<?php echo base_url(); ?>/about">About</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <div class="container">
