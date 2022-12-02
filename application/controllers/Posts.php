b46daf093547c2b45dca4509cc41dcb545e02c9d
dwol87 <dwolinsky87@gmail.com>
2022-12-01 21:31:19 -0500

    power outages galore

--- application/controllers/Posts.php
+++ application/controllers/Posts.php
@@ -11,4 +11,28 @@ class Posts extends CI_Controller
         $this->load->view('posts/index', $data);
         $this->load->view('templates/footer');
     }
+
+    public function view($slug = NULL)
+    {
+        $data['post'] = $this->post_model->get_posts($slug);
+
+        if (empty($data['post'])) {
+            show_404();
+        }
+
+        $data['title'] = $data['post']['title'];
+
+        $this->load->view('templates/header');
+        $this->load->view('posts/view', $data);
+        $this->load->view('templates/footer');
+    }
+
+    public function create()
+    {
+        $data['title'] = 'Create Post';
+
+        $this->load->view('templates/header');
+        $this->load->view('posts/create', $data);
+        $this->load->view('templates/footer');
+    }
 }
