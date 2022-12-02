a684bdaf53a70d220a499b984bae719c4e14a3b1
dwol87 <dwolinsky87@gmail.com>
2022-12-01 23:10:16 -0500

    Crud complete, style needed

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -2,21 +2,21 @@
 
 <head>
     <title>ciBlog</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootswatch@4.5.2/dist/flatly/bootstrap.min.css" integrity="sha384-qF/QmIAj5ZaYFAeQcrQ6bfVMAh4zZlrGwTPY7T/M+iTTLJqJBJjwwnsE5Y0mV7QK" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/style.css">
+    <script src="https://cdn.ckeditor.com/4.20.1/standard/ckeditor.js"></script>
 </head>
 
 <body>
     <nav class="navbar navbar-expand-lg bg-primary">
-        <div class="container">
-            <div class="navbar-header">
-                <a href="<?php echo base_url(); ?>" class="navbar-brand">ciBlog</a>
-            </div>
-            <div id="navbar">
-                <ul class="nav navbar-nav">
-                    <li><a href="<?php echo base_url(); ?>">Home</a></li>
-                    <li><a href="<?php echo base_url(); ?>about">About</a></li>
-                    <li><a href="<?php echo base_url(); ?>posts">Blog</a></li>
+        <div class="container-fluid">
+            <a href="<?php echo base_url(); ?>" class="navbar-brand text-light">ciBlog</a>
+            <div class="navbar">
+                <ul class="navbar-nav">
+                    <li class="nav-item"><a class="nav-link text-light" aria-current="page" href="<?php echo base_url(); ?>">Home</a></li>
+                    <li class="nav-item"><a class="nav-link text-light" href="<?php echo base_url(); ?>about">About</a></li>
+                    <li class="nav-item"><a class="nav-link text-light" href="<?php echo base_url(); ?>posts">Blog</a></li>
+                    <li class="nav-item"><a class="nav-link text-light" href="<?php echo base_url(); ?>posts/create">Create Post</a></li>
                 </ul>
             </div>
         </div>
